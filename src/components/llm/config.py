b344190846b5abd5ba555f477bc9cf4a694830ab
Matheus <matheusbitaraesdenovaes@gmail.com>
2026-03-04 11:20:17 -0300

    change model

--- src/components/llm/config.py
+++ src/components/llm/config.py
@@ -29,15 +29,15 @@ def get_model_config() -> Tuple[str, List[str]]:
     if os.getenv("OPENROUTER_API_KEY"):
         # go here to update the list: https://openrouter.ai/models?fmt=cards&input_modalities=text&max_price=0&order=top-weekly
         openrouter_models = [
-            "openrouter/nvidia/nemotron-nano-12b-v2-vl:free",
-            "openrouter/nvidia/nemotron-nano-9b-v2:free",
-            "openrouter/stepfun/step-3.5-flash:free",
-            "openrouter/z-ai/glm-4.5-air:free",
             "openrouter/deepseek/deepseek-r1-0528:free",
             "openrouter/nvidia/nemotron-3-nano-30b-a3b:free",
             "openrouter/openai/gpt-oss-120b:free",
             "openrouter/arcee-ai/trinity-mini:free",
         ]
+
+        #    "openrouter/nvidia/nemotron-nano-9b-v2:free",
+        #     "openrouter/stepfun/step-3.5-flash:free",
+        #     "openrouter/z-ai/glm-4.5-air:free",
             
         if primary is None:
             primary = openrouter_models[0]
